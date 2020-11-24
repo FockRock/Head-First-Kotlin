376be632f87b64167162d1ed731f2e6f26bfcac5
FockRock <smirnov_nikita@mail.ru>
2020-11-24 16:17:02 +0300

    Lesson 2 updated

--- src/lesson2/PhraseOMatic.kt
+++ src/lesson2/PhraseOMatic.kt
@@ -1,17 +1,15 @@
 package lesson2
 
+import kotlin.random.Random
+
 fun main() {
     val wordArray1 = arrayOf("24/7", "Multi-tier", "B-to-B", "Dynamic", "Pervasive")
     val wordArray2 = arrayOf("Empowered", "Leveraged", "Aligned", "Targeted")
     val wordArray3 = arrayOf("Process", "Paradigm", "Solution", "Portal", "Vision")
 
-    val arraySize1 = wordArray1.size
-    val arraySize2 = wordArray2.size
-    val arraySize3 = wordArray3.size
-
-    val rand1 = (Math.random() * arraySize1).toInt()
-    val rand2 = (Math.random() * arraySize2).toInt()
-    val rand3 = (Math.random() * arraySize3).toInt()
+    val rand1 = Random.nextInt(until = wordArray1.size)
+    val rand2 = Random.nextInt(until = wordArray2.size)
+    val rand3 = Random.nextInt(until = wordArray3.size)
 
     val phrase = "${wordArray1[rand1]} ${wordArray2[rand2]} ${wordArray3[rand3]}"
     println(phrase)
