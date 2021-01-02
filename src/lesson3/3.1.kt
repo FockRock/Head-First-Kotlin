d4464d1c375b47d432aeda909c755573562f862f
FockRock <smirnov_nikita@mail.ru>
2021-01-02 16:40:20 +0300

    Lesson 3 finish!

---
+++ src/lesson3/3.1.kt
@@ -0,0 +1,17 @@
+package lesson3
+
+fun main() {
+    var x = 0
+    var y = 20
+    for (outer in 1..3) {
+        for (inner in 4 downTo 2) {
+
+
+
+            y ++
+            x += 3
+        }
+        y -= 2
+    }
+    println("$x $y")
+}
--- src/lesson3/RockPaperScissors.kt
+++ src/lesson3/RockPaperScissors.kt
@@ -1,9 +1,46 @@
 package lesson3
 
+import kotlin.random.Random
+
 fun main() {
-    val options = arrayOf("Paper", "Scissors", "Rock ")
-    val choice = getGameChoice(options)
+    val options = arrayOf("Paper", "Scissors", "Rock")
+    val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>) =
-        optionsParam[(optionsParam.size * Math.random()).toInt()]
+        optionsParam[Random.nextInt(optionsParam.size)]
+
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+
+    while (!isValidChoice) {
+        print("Please enter one of the following: ")
+        for (item in optionsParam) print(" $item")
+        println(".")
+
+        var userInput = readLine().toString()
+        userInput = userInput.capitalize()
+
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        } else println("You must enter a valid choice.")
+    }
+
+    return userChoice
+}
+
+fun printResult (userChoice: String, gameChoice: String) {
+    val result: String
+
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Scissors" && gameChoice == "Paper") ||
+            (userChoice == "Paper" && gameChoice == "Rock"))
+                result = "You win!"
+    else result = "You lose!"
+    println("You choose $userChoice. I choose $gameChoice. $result")
+}
